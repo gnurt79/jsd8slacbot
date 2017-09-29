783f2f4e57a339df3facde11b3a90fc9e39ee59a
Trung Lac <gnurt79@yahoo.com>
2017-09-29 10:29:43 -0700

    Added tea time functionality

--- scripts/slackbot.js
+++ scripts/slackbot.js
@@ -35,19 +35,45 @@ If you want to supplement your existing code above with any the code below,
 you need to move the contents of module.exports below into the module.exports code above.
 */
 
+// var currentTime = new Date();
+// var hoursToTeaTime;
+var minutesToTeaTime;
+const TEA_TIME_HOURS = 15;
+const TEA_TIME_MINUTES = 50;
 
-var squirrels;
-squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
-
-module.exports = function(robot) {
+module.exports = function(teabot) {
   // Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!"
-  robot.respond(/hi|1hello/i, function(msg) {
-    return msg.send("Howdy!");
+  teabot.respond(/time/i, function(msg) {
+    var currentTime = new Date();
+
+    hoursToTeaTime = currentTime.getHours() - TEA_TIME_HOURS;
+    minutesToTeaTime = currentTime.getMinutes() - TEA_TIME_MINUTES;
+
+    if (hoursToTeaTime < 0 && minutesToTeaTime < 0) {
+      return msg.send("1: The next tea time is in " + Math.abs(hoursToTeaTime) + " hours and " +  Math.abs(minutesToTeaTime) + " minutes.");
+    }
+    else if (hoursToTeaTime >= 0 && minutesToTeaTime < 0) {
+      return msg.send("2: The next tea time is in " + (24 - hoursToTeaTime) + " hours and " +  Math.abs(minutesToTeaTime) + " minutes.");
+    }
+    else if (hoursToTeaTime < 0 && minutesToTeaTime >= 0) {
+      return msg.send("2: The next tea time is in " + Math.abs(hoursToTeaTime) + " hours and " +  (60 - minutesToTeaTime) + " minutes.");
+    }
+    else {
+      return msg.send("2: The next tea time is in " + (24 - hoursToTeaTime) + " hours and " +  (60 - minutesToTeaTime) + " minutes.");
+    }
+
   });
 
-  // Random Example
-  //If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers
-  robot.hear(/ship it/i, function(msg) {
-    return msg.send(msg.random(squirrels));
-  });
+  // teabot.hear(/bot|robot/, function(res) {
+  //  return res.send("I'm a little Teabot!");
+  // });
+  //
+  // bot.respond(/Hi Yellobot! My name is (.*)/i, function(msg) {
+  // var name;
+  // name = msg.match[1];
+  // if (name == "Yellowbot"){
+  //   return msg.send("You're not Yellowbot--I'm Yellowbot!");
+  // } else {
+  //   return msg.reply("Nice to meet you, " + name + "!");
+  // }
 };
