fb8052a109b959fc162792e65a54d971022f8aa3
Trung Lac <gnurt79@yahoo.com>
2017-10-02 18:21:43 -0700

    Added bot for listening

--- scripts/slackbot.js
+++ scripts/slackbot.js
@@ -1,5 +1,7 @@
 // Description:
-// <description of this script's functionality>
+// My hubot is supposed to report the amount remaining intil the next JavaScript class.  It functionally works, but the
+// math on the time calculation is wrong.  I need to use another math algorithm. You can also ask it who is his most or
+// least favorite movie robot.  If it hears "robot" it will respond back to the user with a giphy of a robot.
 //
 // Dependencies:
 //   None
@@ -13,13 +15,13 @@
 //  hears "fail" - returns "I'm a little teabot"
 //  hears "bot" or "robot" - returns a random gif of robots
 //
-// Notes:
+// Notes: The math on the function to return the time of the next JSD class is still funky.
+// But if you are an early bird type then the math is rock solid.
 //
 //
 // Author: gnurt79
 // <github username of the original script author>
 //
-
 // module.exports = function(robot) {
 //
 // }
@@ -37,9 +39,11 @@ If you want to supplement your existing code above with any the code below,
 you need to move the contents of module.exports below into the module.exports code above.
 */
 
+// Constants for keeping track of what time the JSD class starts
 const TEA_TIME_HOURS = 18;
 const TEA_TIME_MINUTES = 30;
 
+// Arrays for holding images of good and bad robots
 mostFavRobotImages = [
   "http://loadion.com/ii/4504413206_ddd10b28d9.jpg",
   "https://cdn.pastemagazine.com/www/articles/19-Best-100-Robots-in-Film-Robot-WallE.jpg",
@@ -54,17 +58,16 @@ leastFavRobotImages = [
   "http://pop.h-cdn.co/assets/cm/15/05/54caec985a61e_-_evil-robots-00-0612-de.jpg"
 ];
 
+// Object for the Giphy API
 const giphy = {
   api_key: process.env.HUBOT_GIPHY_API_KEY,
   base_url: 'http://api.giphy.com/v1',
-
 };
 
-
+// Function to retrieve a random gif grom Giphy
 var search = function(msg, query, cb) {
   const endpoint = '/gifs/search';
   const url = `${giphy.base_url}${endpoint}`;
-
   return msg.http(url)
     .query({
       q: query,
@@ -78,24 +81,27 @@ var search = function(msg, query, cb) {
           // return cb(image.images.original.url);
           return msg.reply(image.images.downsized.url);
         }
-
       } catch (e) {
         response = undefined;
         // return cb('Error');
         return msg.reply(`No results found for ${query}`);
       }
-
       if (response === undefined) { return; }
   });
 };
 
 module.exports = function(teabot) {
-  // Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!"
+
+  // Teabot when return the number of days, hours, and minutes before the next JSD class
   teabot.respond(/When is the next jsd class/i, function(msg) {
+
+    // Variables for tracking time
     var currentTime = new Date();
     var daysToTeaTime;
     var hoursToTeaTime = currentTime.getHours() - TEA_TIME_HOURS;
     var minutesToTeaTime = currentTime.getMinutes() - TEA_TIME_MINUTES;
+
+    // Object for storing the month and day of JSD class
     var jsdClasses = {
       october: [2, 4, 9, 11, 16, 18, 23, 25, 30],
       november: [1, 6, 8, 13],
@@ -123,7 +129,11 @@ module.exports = function(teabot) {
         }
       }
     };
+
+    // Run function to retrieve the day of the next class
     jsdClasses.nextClass();
+
+    // Conditions to calculate the hours and minutes before the next class
     if (hoursToTeaTime < 0 && minutesToTeaTime < 0) {
       return msg.send("1: The next JSD class is in " + daysToTeaTime + " day(s), " +  Math.abs(hoursToTeaTime) + " hours, and " +  Math.abs(minutesToTeaTime) + " minutes.");
     }
@@ -138,10 +148,7 @@ module.exports = function(teabot) {
     }
   });
 
-  // teabot.hear(/fail/, function(res) {
-  //  return res.send("I'm a little Teabot!");
-  // });
-
+  // Teabot will respond to user's question and return an image of most or least favorite robot
   teabot.respond(/who is your (.*) favorite robot/i, function(msg) {
    var fav;
    fav = msg.match[1];
@@ -156,7 +163,9 @@ module.exports = function(teabot) {
        return msg.send("ME OF COURSE!!");
    }
   });
-  teabot.hear(/robot/i, function(msg) {
+
+  // Teabot will reply to user if "robot" is mention in the channel
+  teabot.hear(/bot|robot/i, function(msg) {
     search(msg, "robot");
   });
 };
